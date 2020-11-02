22a9accca171cd1a3a75fe2cf7ecbebf372bd587
wpramio <willianpramio@hotmail.com>
2020-11-02 19:39:35 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,18 +3,25 @@ from track import Track
 
 class Generator:
 
-    def __init__(self, text, musicName):
-        self.text = text
-        self.track = Track(1)
-        self.musicName = musicName
+  def __init__(self, text, musicName, initialBPM, initialInstrument):
+    self._text = text
+    self._instructions = Translator().translate_text_to_instructions(self._text)
+    print('Array de instrucoes:', self._instructions)
+    self._track = Track(1)
+    self._musicName = musicName
+    self._initial_bpm = initialBPM
+    self._initialInstrument = initialInstrument
 
-    def generate_song(self):
-        self.track.addNote(60, 100)
-        self.track.addNote(64, 100)
-        self.track.addNote(67, 100)
-        self.track.changeInstrument(81)
-        self.track.addNote(60, 100)
-        self.track.addNote(64, 100)
-        self.track.addNote(67, 100)
+  def generate_song(self):
+    for instruction in self._instructions:
+      method = instruction[0]
+      arg = instruction[1]
+      callTrackMethod = getattr(self._track, method, 'repeatNote')
+      print(callTrackMethod)
+      callTrackMethod(arg)
+    self._track.finishMusic(self._musicName)
 
-        self.track.finishMusic(self.musicName)
+if __name__ == "__main__":
+  texto = "CDE9FGABD"
+  generator = Generator(texto, 'Testando_generator', 120, 100)
+  generator.generate_song()
